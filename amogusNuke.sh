131388e3b73a0acf353ab8b24150f5b07bb19419
Qnec <81662803+Qnec@users.noreply.github.com>
2021-08-30 22:04:49 -0700

    Add files via upload

--- amogusNuke.sh
+++ amogusNuke.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl https://images.gamebanana.com/img/ico/sprays/603980092d87f.png --output amogus.png
-find $1 -type f -print0 | xargs -0 bash -c 'for arg do file -b --mime-type "$arg" | grep -q -P '^image\/\\\(?!\\\(svg\\\)\\\)' && echo "$arg"; done' _ | while read file
+find $1 -type f -print0 | xargs -0 bash -c 'for arg do file -b --mime-type "$arg" | grep -q -P '^image\/\\\(\\\(gif\\\)\\\|\\\(jpeg\\\)\\\|\\\(png\\\)\\\|\\\(tiff\\\)\\\|\\\(webp\\\)\\\|\\\(bmp\\\)\\\)' && echo "$arg"; done' _ | while read file
 do
   echo $file
-  convert amogus.png -resize $(identify -ping -format '%wx%h!' "$file") "$file"
+  #convert amogus.png -resize $(identify -ping -format '%wx%h!' "$file") "$file"
 done
